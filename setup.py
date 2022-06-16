17a39789901dea17fa52777391a88df9cb6e0d4e
Yiagos <yiagoskyrits@gmail.com>
2022-06-16 18:49:52 +0100

    Library Files

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 from setuptools import find_packages, setup
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering',
+
+
+]
+
 setup(
     name='postfoam',
     packages=find_packages(include=['postfoam']),
     version='0.1.0',
-    description='My first Python library',
-    author='Me',
+    description='A python library for postporcessing openfoam simulations.',
+    long_description=open('README.md').read(),
+    author='Ioannis Kyritsopoulos',
+    author_email='yiagoskyrits@gmail.com',
+    keywords=['OpenFOAM', 'postprocessing'],
+    classifiers=classifiers,
+    install_requires=['numpy','array','matplotlib','scipy','re'],
     license='MIT',
 )
